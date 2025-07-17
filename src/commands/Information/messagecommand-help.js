f1c32bfe7be20ca8a56c0b72ce2399e44a83eb8e
Pandipipas <pablocg182000@gmail.com>
2025-07-17 12:02:53 +0200

    feat: improve message and slash help command layout with embed formatting

--- src/commands/Information/messagecommand-help.js
+++ src/commands/Information/messagecommand-help.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require("discord.js");
 const { Message } = require("discord.js");
 const DiscordBot = require("../../client/DiscordBot");
 const MessageCommand = require("../../structure/MessageCommand");
@@ -29,10 +30,22 @@ module.exports = new MessageCommand({
             prefix = client.database.ensure('prefix-' + message.guild.id, config.commands.prefix);
         }
 
-        const commands = client.collection.message_commands.map((cmd) => `\`${prefix}${cmd.command.name}\``).join(', ');
+        const embed = new EmbedBuilder()
+            .setTitle('📖 Help - Message Commands')
+            .setDescription(`Here is a list of available commands using prefix: \`${prefix}\``)
+            .setColor(0x5865F2)
+            .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+            .addFields(
+                client.collection.message_commands.map((cmd) => ({
+                    name: `\`${prefix}${cmd.command.name}\``,
+                    value: cmd.command.description || 'No description provided.',
+                }))
+            )
+            .setFooter({
+                text: `Requested by ${message.author.tag}`,
+                iconURL: message.author.displayAvatarURL({ dynamic: true })
+            });
 
-        await message.reply({
-            content: `Available commands: ${commands}`
-        });
+        await message.reply({ embeds: [embed] });
     }
 }).toJSON();
--- src/commands/Information/slashcommand-help.js
+++ src/commands/Information/slashcommand-help.js
@@ -1,11 +1,11 @@
-const { ChatInputCommandInteraction } = require("discord.js");
+const { ChatInputCommandInteraction, EmbedBuilder, MessageFlags } = require("discord.js");
 const DiscordBot = require("../../client/DiscordBot");
 const ApplicationCommand = require("../../structure/ApplicationCommand");
 
 module.exports = new ApplicationCommand({
     command: {
         name: 'help',
-        description: 'Replies with a list of available application commands.',
+        description: 'Shows a list of available slash commands.',
         type: 1,
         options: []
     },
@@ -18,8 +18,25 @@ module.exports = new ApplicationCommand({
      * @param {ChatInputCommandInteraction} interaction 
      */
     run: async (client, interaction) => {
+        const embed = new EmbedBuilder()
+            .setTitle('📖 Help - Slash Commands')
+            .setColor(0x5865F2)
+            .setThumbnail(client.user.displayAvatarURL())
+            .setFooter({
+                text: `Requested by ${interaction.user.tag}`,
+                iconURL: interaction.user.displayAvatarURL({ dynamic: true })
+            });
+
+        client.rest_application_commands_array.forEach(cmd => {
+            embed.addFields({
+                name: `\`/${cmd.name}\``,
+                value: cmd.description || 'No description',
+            });
+        });
+
         await interaction.reply({
-            content: `${client.collection.application_commands.map((cmd) => '\`/' + cmd.command.name + '\`').join(', ')}`
+            embeds: [embed],
+            flags: 64
         });
     }
 }).toJSON();
